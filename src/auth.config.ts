098cc798b4d8e6105eb7f26b61b1ce0b6cde4db1
Your Name <you@example.com>
2026-08-26 04:55:30 -0600

    feat: agregar configuración de autenticación

---
+++ src/auth.config.ts
@@ -0,0 +1,31 @@
+import type { NextAuthConfig } from "next-auth";
+
+const authConfig = {
+  pages: {
+    signIn: "/login",
+  },
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+        token.role = user.role;
+      }
+
+      return token;
+    },
+    session({ session, token }) {
+      if (session.user && token.id && token.role) {
+        session.user.id = token.id;
+        session.user.role = token.role;
+      }
+
+      return session;
+    },
+  },
+  providers: [],
+} satisfies NextAuthConfig;
+
+export default authConfig;
